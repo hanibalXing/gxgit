5d469baf31afba5b41d6c44c8a69794428000b8e
gxxgithub <gx1984104@126.com>
2019-03-11 01:20:12 +0800

    basic

---
+++ src/main/java/cn/gx/storm/trident2/opaque/OpaqueTridentSpout.java
@@ -0,0 +1,36 @@
+package cn.gx.storm.trident2.opaque;
+
+import cn.gx.storm.trident2.partitioned.MetaData;
+import cn.gx.storm.trident2.partitioned.Partition;
+import org.apache.storm.task.TopologyContext;
+import org.apache.storm.trident.spout.IOpaquePartitionedTridentSpout;
+import org.apache.storm.tuple.Fields;
+
+import java.util.Map;
+
+public class OpaqueTridentSpout implements IOpaquePartitionedTridentSpout<Map<String, Integer>, Partition, MetaData>
+{
+    @Override
+    public Emitter<Map<String, Integer>, Partition, MetaData> getEmitter(Map conf, TopologyContext context)
+    {
+        return new OpaqueTridentEmitter(context);
+    }
+
+    @Override
+    public Coordinator<Map<String, Integer>> getCoordinator(Map conf, TopologyContext context)
+    {
+        return new OpaqueTridentCoordinator();
+    }
+
+    @Override
+    public Map<String, Object> getComponentConfiguration()
+    {
+        return null;
+    }
+
+    @Override
+    public Fields getOutputFields()
+    {
+        return new Fields("txId", "number");
+    }
+}
---
+++ src/main/java/cn/gx/storm/trident2/partitioned/Partition.java
@@ -0,0 +1,51 @@
+package cn.gx.storm.trident2.partitioned;
+
+import org.apache.storm.trident.spout.ISpoutPartition;
+
+import java.io.Serializable;
+
+public class Partition implements ISpoutPartition, Serializable
+{
+
+    private final String id;
+
+    public Partition(String id)
+    {
+        this.id = id;
+    }
+
+    @Override
+    public String getId()
+    {
+        return id;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Partition{" +
+                "id='" + id + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        Partition partition = (Partition) o;
+
+        return id != null ? id.equals(partition.id) : partition.id == null;
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return id != null ? id.hashCode() : 0;
+    }
+}
